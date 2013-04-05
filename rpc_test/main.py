73d901d0c3d8ffb924a3f5300fcc6a2b1e024027
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-04 21:47:50 -0500

    take 3

--- rpc_test/main.py
+++ rpc_test/main.py
@@ -1,6 +1,7 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from multiprocessing import Process
 import urllib
+import json
 
 HOST = 'localhost'
 # 1 - 65535
@@ -9,11 +10,16 @@ PORT = 65456
 def connect(host, port, game_number):
     print(host, port, game_number)
 
+test = {
+    'a': 3231,
+}
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
         if self.path.startswith('/?'):
-            self.wfile.write(urllib.unquote(self.path[2:]))
+            self.wfile.write('jsonCallback({0});'.format(json.dumps(test)))
+            #self.wfile.write(urllib.unquote(self.path[2:]))
         else:
             self.wfile.write("No Command Recieved!\nI'll just wait here.")
 
