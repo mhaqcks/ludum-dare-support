67568f2851d6d944678b7c4abb10eef3bdcc4fd4
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-05 01:02:40 -0500

    Ludum handler

--- rpc_test/main.py
+++ rpc_test/main.py
@@ -5,59 +5,61 @@ import json
 import ssl
 import urlparse
 
-# Warn about monkey in the middle attacks.
 
+class RPCHandler(BaseHTTPRequestHandler):
 
-class SecureHTTPServer(HTTPServer):
-    def __init__(self, server_address, HandlerClass):
-        BaseServer.__init__(self, server_address, HandlerClass)
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        #server.pem's location (containing the server private key and
-        #the server certificate).
-        fpem = './server.pem'
-        ctx.use_privatekey_file (fpem)
-        ctx.use_certificate_file(fpem)
-        self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
-                                                        self.socket_type))
-        self.server_bind()
-        self.server_activate()
+    functions = {}
 
-HOST = 'localhost'
-# 1 - 65535
-PORT = 65456
-
-def connect(host, port, game_number):
-    print(host, port, game_number)
-
-test = {
-    'a': 3231,
-}
-
-class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
         if self.path.startswith('/?'):
             self.wfile.write('jsonCallback({0});'.format(json.dumps(test)))
             self.wfile.write('\n')
-            # self.wfile.write(urlparse.urlparse(self.path).query)
-            self.wfile.write(urllib.unquote(self.path[2:]))
+            query = urlparse.urlparse(self.path).query
+
+            variables = {}
+            for statement in query.split('&'):
+                name, value = statement.split('=', 1)
+                variables[name] = value
+
+            output = json.loads(urllib.unquote(variables['json']))
+
+            if output['method'] not in self.functions:
+                self.wfile.write('This command has not been deemed safe by the overlords!')
+            else:
+                self.functions[output['method']](**output['args'])
+                self.wfile.write(output)
+
         else:
             self.wfile.write("No Command Recieved!\nI'll just wait here.")
 
-def rpc_server():
 
-    server = HTTPServer((HOST, PORT), Handler)
-    # SSL
-    server.socket = ssl.wrap_socket(server.socket, certfile='./server.pem', server_side=True)
+class LudumProcedureCallServer(self):
+    def __init__(self, host, port, certfile='./server.pem'):
+        self.host = host
+        self.port = port
+        self.certfile = certfile
+        self.functions = {}
+        self.handler = RPCHandler
 
-    server.serve_forever()
+    def register_function(self, function, function_name=None):
+        if function_name is None:
+            function_name = function.__name__
 
-def main():
-    p = Process(target=rpc_server)
-    p.start()
+        self.handler.functions[function_name] = function
 
-    p.join()
+    def start(self):
+        self.server = HTTPServer((self.host, self.port), self.handler)
+        # SSL
+        self.server.socket = ssl.wrap_socket(
+            server.socket, certfile=self.certfile, server_side=True)
 
+        self.server.serve_forever()
 
+# Warn about monkey in the middle attacks.
 if __name__ == '__main__':
-    main()
+    HOST = 'localhost'
+    # 1 - 65535
+    PORT = 65456
+
+    rpc = LudumProcedureCallServer(HOST, PORT)
