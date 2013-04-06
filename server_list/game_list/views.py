3b8226b8d91c1dd5f86dc4679172332192b1974e
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-06 01:50:11 -0500

    Filters are now working

--- server_list/game_list/views.py
+++ server_list/game_list/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import os
 import random
+import json
 
 FILE_ROOT = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
 
@@ -42,6 +44,66 @@ def download(request):
 
     return render(request, 'download.html', context)
 
+JSON = [
+    {
+        "game_number": -1,
+        "game_name": "This is a game name!!",
+        "players": "2/5",
+        "status": "Particularly Slutty",
+        "locked": True
+    },
+    {
+        "game_number": -2,
+        "game_name": "This is not a game name!!",
+        "players": "5/5",
+        "status": "Ummm... Good, I guess.",
+        "locked": False
+    },
+]
+
 
 def games(request):
-    return render(request, 'gamelist.json', content_type='application/json')
+
+    content = JSON
+    game_number = request.GET['game_number']
+    game_name = request.GET['game_name'].lower()
+    players = request.GET['players'].lower()
+    locked = request.GET['locked'].lower()
+    print(game_number, game_name, players, locked)
+    if game_number:
+        game_number = int(game_number)
+        for row in content:
+            if int(row['game_number']) == game_number:
+                content = [row]
+                break
+
+    rows = []
+    if game_name:
+        for row in content:
+            if game_name in row['game_name'].lower():
+                rows += [row]
+
+        content = rows
+
+    rows = []
+    if players == 'hide full games':
+        for row in content:
+            n, m = row['players'].split('/')
+            if n != m:
+                rows += [row]
+
+        content = rows
+
+    rows = []
+    if locked != 'all':
+        for row in content:
+            if locked == 'public' and not row['locked']:
+                rows += [row]
+            elif locked == 'private' and row['locked']:
+                rows += [row]
+
+        content = rows
+
+    content = json.dumps({'gamedata': content})
+    return HttpResponse(content, content_type='application/json')
+    #return render(request, 'gamelist.json', content_type='application/json')
