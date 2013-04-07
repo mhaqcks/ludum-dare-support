ec365f98eef21e68a29fd43b3dd1b48ef401f056
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-07 15:18:11 -0500

    Tubby Tom

--- django/game_list/bios.py
+++ django/game_list/bios.py
@@ -78,7 +78,7 @@ TubbyTom = Bio(
     'Thomas S. Coleman',
     'Art Consultant, Consumer',
     'colemant.png',
-    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+    "Pitchfork craft beer wayfarers sartorial cred, mcsweeney's synth umami letterpress keffiyeh +1 high life photo booth vegan YOLO. Irony vice fashion axe tattooed kale chips, chillwave 3 wolf moon deep v hoodie farm-to-table authentic pinterest mlkshk. Single-origin coffee disrupt sustainable yr semiotics jean shorts. Aesthetic occupy bushwick swag. Readymade cred sartorial selvage semiotics disrupt craft beer, tousled +1 mustache. Flexitarian sustainable stumptown keffiyeh, dreamcatcher blog vinyl jean shorts. Put a bird on it tattooed biodiesel, dreamcatcher ethical odd future next level flannel keffiyeh.")
 
 JustinKellogg = Bio(
     'Justin M. Kellogg',
