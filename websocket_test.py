c2185ba019971c2e8171cda295516a83ef8a1343
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-07 01:07:06 -0500

    Notifications

---
+++ websocket_test.py
@@ -0,0 +1,22 @@
+import tornado.ioloop
+from tornado import websocket
+
+
+class EchoWebSocket(websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+application = tornado.web.Application([
+    (r"/", EchoWebSocket),
+])
+
+if __name__ == "__main__":
+    application.listen(65456)
+    tornado.ioloop.IOLoop.instance().start()
