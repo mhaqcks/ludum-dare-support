354fc996fffc83819f072ebf644f769489ff3335
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-07 14:48:33 -0500

    Bios are in, now

---
+++ django/game_list/bios.py
@@ -0,0 +1,124 @@
+
+class Bio(object):
+    def __init__(self, name, title, img, desc):
+        self.name = name
+        self.title = title
+        self.img = img
+        self.desc = desc
+
+
+JacobGardner = Bio(
+    'Jacob V. Gardner',
+    'Project Lead, Sragon Dlayer',
+    'gardnerj.png',
+    'Born and raised in the lost city of Atlantis, Jacob represents SEVERAL hours of research and ingenuity.  Currently in the employ of Garmin International, Inc., he writes software that goes into aircraft displays and develops tools to assist developers in increasing their testing efficiency.   He is a graduate of Computer Science from Missouri University of Science & Technology, formerly Unversity of Missouri - Rolla, formerly University of Missouri at Rolla, formerly the Missouri School of Mines and Metallurgy. In his free time, he feeds the hungry (himself), watches an embarassingly large amount of Netflix, and complains about Microsoft and EA Games.  He also loves dogs.' )
+
+EmilyGannon = Bio(
+    'Emily M. Gannon',
+    'Writer, Actress, Alpha Tester',
+    'gannone.png',
+    '''Emily is an avid time traveler but has learned nothing from the future except how to fly.  She is currently in pursuit of a edumaction degree fer teachin' Ingrris.  Emily spends a shameful amount of time snuggling with one cuddly, gray kitty named Misty and the rest of her time looking at pictures of cats doing funny things on the internet. Her Bachelors of Arts degrees in English and Theatre have gotten her nothing thus far except for more years in school. Some may call her a hipster for her interests in recycling, music, gardening, and acting but she "isn't a soup can, man."  You can draw her from her cave of introversion with all chocolates except for dark or the promise to help a cute animal.''')
+
+NathanReynolds = Bio(
+    'Nathan T. Reynolds',
+    'Publisher, Pterodactyl',
+    'reynoldsn.png',
+    'Cras consectetur lacinia varius. Aliquam et erat id urna malesuada malesuada consequat vitae diam. Vestibulum ullamcorper faucibus velit id porta. Mauris nulla nisi, consectetur sed ultrices et, commodo vel elit. Nunc vulputate pharetra massa a commodo. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed volutpat posuere magna et commodo. Proin venenatis vestibulum dolor, sit amet feugiat nulla ultrices id. Curabitur enim ipsum, rutrum ac euismod ut, mollis eget urna.')
+
+KaitlynnJansen = Bio(
+    'Kaitlynn E. Jansen',
+    'Mathematics, Wiles',
+    'jansenk.png',
+    "This page is required to post a comment. Each arc of the main lake, but grief. Lion's soup, it's the author's, not here too. Now a hotel More vehicles to large porch systems. Contact information for the impact your investment. Contact Us About this afternoon before the tank. Welcome to hendrerit bottom of the column temperature, before the time in the convenience of life. More times than today's official website. Admin Home My vehicles and Japan's price Community. As long as she is with child by fear of the yeast needs. It's more dynamic. Contact.")
+
+RyanSchmitt = Bio(
+    'Clayton R. Schmitt',
+    'Creative Director, Miracle Child',
+    'schmittr.png',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem nulla. Aliquam scelerisque erat enim, a porttitor est. Nunc lectus massa, ullamcorper eget fermentum a, dignissim eu dolor. Donec nec neque metus, ac vehicula erat. Maecenas quis mi quis arcu dictum hendrerit luctus egestas ligula. Aliquam non sagittis leo. Sed rutrum purus nec libero elementum a sollicitudin augue dictum. In vel orci ac orci porta rutrum. Etiam id cursus quam. Sed sit amet sem in mi iaculis convallis at eget felis. Aliquam erat volutpat. Vestibulum luctus malesuada vestibulum. Maecenas sapien libero, dictum convallis vulputate vel, dictum at lorem. Donec lacinia turpis non purus pretium nec viverra mauris lobortis.' )
+
+BobbySanders = Bio(
+    'Bobby H. Sanders',
+    'Developer, Person, Star',
+    'sandersb.png',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem nulla. Aliquam scelerisque erat enim, a porttitor est. Nunc lectus massa, ullamcorper eget fermentum a, dignissim eu dolor. Donec nec neque metus, ac vehicula erat. Maecenas quis mi quis arcu dictum hendrerit luctus egestas ligula. Aliquam non sagittis leo. Sed rutrum purus nec libero elementum a sollicitudin augue dictum. In vel orci ac orci porta rutrum. Etiam id cursus quam. Sed sit amet sem in mi iaculis convallis at eget felis. Aliquam erat volutpat. Vestibulum luctus malesuada vestibulum. Maecenas sapien libero, dictum convallis vulputate vel, dictum at lorem. Donec lacinia turpis non purus pretium nec viverra mauris lobortis.')
+
+JimHarter = Bio(
+    'James R. Harter III',
+    'Musician, Jim Harter Impersonator',
+    'harterj.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+KevinGrob = Bio(
+    'Kevin A. Grob',
+    'Lava Lamp',
+    'grobk.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+
+MaxEisenbraun = Bio(
+    'Max T. Eisenbraun',
+    'Software Lead, Beard',
+    'eisenbraunm.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+ScottWright = Bio(
+    'Scott A. Wright',
+    'Mock Interviewer',
+    'wrights.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+AdamLucz = Bio(
+    'Adam S. Lucz',
+    'Drummer Dude, Mocap Specialist',
+    'lucza.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+TubbyTom = Bio(
+    'Thomas S. Coleman',
+    'Art Consultant, Consumer',
+    'colemant.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+JustinKellogg = Bio(
+    'Justin M. Kellogg',
+    'Specialist',
+    'kelloggj.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo. ')
+
+ZachLee = Bio(
+    'Zach K. Lee',
+    'Remote Mocap Director',
+    'leez.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+JaredFrisby = Bio(
+    'Jared P. Frisby',
+    'Lulz',
+    'frisbyj.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+JimMahon = Bio(
+    'James Q. Mahon',
+    'Mixed Martial Barfighter',
+    'mahonj.png',
+    'Duis auctor cursus hendrerit. Proin lorem ipsum, tempor ut hendrerit sit amet, luctus fermentum odio. Donec quis urna ac elit accumsan varius. Integer id posuere ipsum. Curabitur id ligula mi. Maecenas ut tellus lacus. Vivamus magna enim, cursus quis malesuada sit amet, pulvinar nec sem. Donec vel tortor mi, quis feugiat elit. Mauris consectetur mauris eget mauris ultrices in fermentum sem malesuada. Proin id massa est. Donec a ligula quis erat tincidunt euismod at venenatis nulla. Nulla dapibus, turpis ut scelerisque malesuada, lorem nulla posuere velit, eu bibendum purus libero id nunc. Quisque ornare, dolor ac dignissim dapibus, nunc risus scelerisque diam, at pharetra nunc enim eget justo.')
+
+BIOS = [
+    JacobGardner,
+    EmilyGannon,
+    NathanReynolds,
+    KaitlynnJansen,
+    RyanSchmitt,
+    BobbySanders,
+    JimHarter,
+    KevinGrob,
+    MaxEisenbraun,
+    ScottWright,
+    AdamLucz,
+    TubbyTom,
+    JustinKellogg,
+    ZachLee,
+    JaredFrisby,
+    JimMahon,
+]
