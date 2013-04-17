19d6436751bc21a5e651b195dde53c6cd7dc856f
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-17 01:30:56 -0500

    Connect info

--- django/game_list/static/js/prj/game_list.js
+++ django/game_list/static/js/prj/game_list.js
@@ -1,28 +1,3 @@
-function get_game(game_number, password) {
-
-	function pre_cunnect(xhr, settings) {
-		set_csrf(xhr, settings);
-	}
-
-	$.ajax({
-		url: game_url,
-		beforeSend: pre_cunnect,
-		data: {
-			'game_number': game_number,
-			'password': password,
-		},
-		type: 'POST',
-		success: function(data, textStatus, jqXHR) {
-			if(data.error != null) {
-				alert(data.error);
-			} else {
-				$('#connect-dialog').modal();
-				console.log(data.connect_info);
-			}
-
-		},
-	});
-}
 
 function add_row(element, index, array)
 {
