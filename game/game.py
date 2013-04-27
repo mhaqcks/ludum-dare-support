d6251d03b683fc595c19c804c1f2ce847ca211d9
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-04-27 13:51:20 -0500

    Game start stuff

---
+++ game/game.py
@@ -0,0 +1,18 @@
+import pyglet
+
+def main():
+    window = pyglet.window.Window(width=1280, height=720)
+
+    level = StartLevel()
+
+    @window.event
+    def on_draw():
+        window.clear()
+        level.draw()
+
+    pyglet.app.run()
+
+
+if __name__ == '__main__':
+    main()
+
